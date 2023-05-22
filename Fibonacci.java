9bd2601c59f0fb81e178c8791526b23d71b8cb7f
MilesWalker726 <123109886+MilesWalker726@users.noreply.github.com>
2023-05-21 20:04:32 -0400

    Add files via upload
    Javadoc notation added.

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 
 public class Fibonacci 
 {
+	/**
+	 * 
+	 * @param num
+	 * @return fibonacci sequence
+	 * 
+	 * A recursive sequence that returns the fibonacci value that the user requested.
+	 */
 
 	 public static int fibonacci(int num)
 	 {
@@ -14,18 +21,31 @@ public class Fibonacci
 	    return fibonacci(num-1) + fibonacci(num-2);
 	 }
 
-	public static void main(String[] args) 
-	{
-		int num;
 
-	    Scanner input = new Scanner(System.in);
+	 
+	 public static void main(String[] args) 
+	 {
+		 /**
+		  * 
+		  * @author Miles Walker
+		  * @param num
+		  * @return
+		  * 
+		  * The main class asked for the user to input the level of the fibonacci sequence that they would like to see.
+		  * It then calls the fibonacci class and gives it the number that the user inputs.
+		  * Finally, it will tell the user what the fibonacci sequence is that they requested.
+		  * 
+		  */
+		 int num;
 
-	    System.out.println("Enter Value of Fibonacci Sequence: ");
-	    num = input.nextInt();
+		 Scanner input = new Scanner(System.in);
 
-	    System.out.println("The " + num + "th term of the Fibonacci Sequence is " + fibonacci(num));
+		 System.out.println("Enter Value of Fibonacci Sequence: ");
+		 num = input.nextInt();
+
+		 System.out.println("The " + num + "th term of the Fibonacci Sequence is " + fibonacci(num));
 	    
-	    input.close();
-	}
+		 input.close();
+	 }
 
 }
